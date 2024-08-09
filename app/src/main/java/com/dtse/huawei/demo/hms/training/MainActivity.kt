f8dfbf84d0f81c38182cdc8137f1fb25c7340876
Armand <armando.gonzalez.ramirez1@huawei.com>
2024-08-09 01:59:35 -0600

    Upload Kits Demo of Scan Analitycs and Account

--- app/src/main/java/com/dtse/huawei/demo/hms/training/MainActivity.kt
+++ app/src/main/java/com/dtse/huawei/demo/hms/training/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity(){
         val arrayAdapter: ArrayAdapter<*>
         val users = arrayOf(
             "HMS Push Kit", "HMS Map Kit", "HMS Analytics Kit",
-            "HMS IAP Kit", "HMS ML Scan Kit"
+            "HMS IAP Kit", "HMS Account Kit", "HMS ML Scan Kit"
         )
 
         // access the listView from xml file
@@ -39,6 +39,9 @@ class MainActivity : AppCompatActivity(){
             when(position) {
                 0 -> intent = Intent(this, PushActivity::class.java)
                 1 -> intent = Intent(this, MapActivity::class.java)
+                2 -> intent = Intent(this, AnalyticsActivity::class.java)
+                4 -> intent = Intent(this, AccountActivity::class.java)
+                5 -> intent = Intent(this, ScanActivity::class.java)
                 else -> {
 
                 }
